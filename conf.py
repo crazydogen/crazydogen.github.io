8691a035282bd87025d7e60fbebb67c7636c076f
crazydogen <ianlee19941128@gmail.com>
2020-09-17 19:54:15 +0800

    update conf.py

--- conf.py
+++ conf.py
@@ -8,10 +8,12 @@ source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
+# remote template 
 template = {
     "name": "Galileo",
-    "type": "local",
-    "path": "../Galileo"
+    "type": "git",
+    "url": "https://github.com/AlanDecode/Maverick-Theme-Galileo.git",
+    "branch": "latest"
 }
 enable_jsdelivr = {
     "enabled": True,
