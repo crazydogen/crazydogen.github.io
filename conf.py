b691021ab118e1781b6d4450bd0aba143396a1e5
crazydogen <ianlee19941128@gmail.com>
2020-09-21 14:43:44 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -27,7 +27,7 @@ site_build_date = "2020-04-19T14:51+08:00"
 author = "CrazyDogen"
 email = "ZVd4cE1USXhNaU52ZFhSc2IyOXJMbU52YlE9PQ=="
 author_homepage = "https://github.com/crazydogen"
-description = "Stay Hungry. Stay Foolish"
+description = "Everything has a trade-off. Think more, do less."
 key_words = ['CrazyDogen','blog']
 language = 'en'
 external_links = [
